52c65680b4b55abb9dbfbf9fd926611b0ceed833
UBUNTU14 <alejandrofernandezb92@gmail.com>
2016-02-28 10:07:51 -0800

    Segunda Version

--- js/gmaps.js
+++ js/gmaps.js
@@ -10,13 +10,6 @@
 
 }(this, function() {
 
-/*!
- * GMaps.js v0.4.16
- * http://hpneo.github.com/gmaps/
- *
- * Copyright 2014, Gustavo Leon
- * Released under the MIT License.
- */
 
 if (!(typeof window.google === 'object' && window.google.maps)) {
   throw 'Google Maps API is required. Please register the following JavaScript library http://maps.google.com/maps/api/js?sensor=true.'
